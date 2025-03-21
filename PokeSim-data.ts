2f22711fedaee7fb626f7d9cacb9d5383adc2174
Trevor Michel Biri <111523767+Draike13@users.noreply.github.com>
2025-03-21 15:40:55 -0500

    Update PokeSim-data.ts
    completed data file, with extra challenges built in for specific battle mechanics

--- PokeSim-data.ts
+++ PokeSim-data.ts
@@ -1,13 +1,381 @@
 pokeData = [
   {
-    id: ,
-    name: ,
-    type: ,
+    id: 0006,
+    name: "Charizard",
+    type: "Fire",
     selected: False,
-    atk: ,
-    def: ,
-    sAtk: ,
-    sDef: ,
-    
+    atk: 150,
+    def: 150,
+    sAtk: null,
+    sDef: 100,
   }
+  {
+    id: 0012,
+    name: "Butterfree",
+    type: "Bug",
+    selected: False,
+    atk: null,
+    def: 30,
+    sAtk: 100,
+    sDef: 50,
+  }
+  {
+    id: 0025,
+    name: "Pikachu",
+    type: "Electric",
+    selected: False,
+    atk: null,
+    def: 80,
+    sAtk: 150,
+    sDef: 80,
+  }
+  {
+    id: 0094,
+    name: "Gengar",
+    type: "Ghost",
+    selected: False,
+    atk: null,
+    def: 200,
+    sAtk: 160,
+    sDef: 50,
+  }
+  // {
+  //   id: 0132,
+  //   name: "Ditto",
+  //   type: "Normal",
+  //   selected: False,
+  //   atk: null,
+  //   def: 100,
+  //   sAtk: //oponents Atk,
+  //   sDef: 100,
+  // }
+  {
+    id: 0133,
+    name: "Eevee",
+    type: "Normal",
+    selected: False,
+    atk: 999,
+    def: 999,
+    sAtk: 999,
+    sDef: 999,
+    //no one defeats Eevee!
+  }
+  {
+    id: 0150,
+    name: "Mewtwo",
+    type: "Psychic",
+    selected: False,
+    atk: null,
+    def: 150,
+    sAtk: 200,
+    sDef: 150,
+  }
+  {
+    id: 0151,
+    name: "Mew",
+    type: "Psychic",
+    selected: False,
+    atk: null,
+    def: 50,
+    sAtk: 250,
+    sDef: 50,
+  }
+  {
+    id: 0249,
+    name: "Lugia",
+    type: "Flying",
+    selected: False,
+    atk: 180,
+    def: 180,
+    sAtk: null,
+    sDef: 180,
+  }
+  {
+    id: 0282,
+    name: "Gardevoir",
+    type: "Psychic",
+    selected: False,
+    atk: null,
+    def: 80,
+    sAtk: 220,
+    sDef: 120,
+  }
+  {
+    id: 0289,
+    name: "Slaking",
+    type: "Normal",
+    selected: False,
+    atk: 300,
+    def: 30,
+    sAtk: null,
+    sDef: 30,
+  }
+  // {
+  //   id: 0292,
+  //   name: "Shedinja",
+  //   type: "Ghost",
+  //   selected: False,
+  //   atk: null,
+  //   def: null,
+  //   sAtk: 10,
+  //   sDef: 10,
+  // }
+  {
+    id: 0334,
+    name: "Altaria",
+    type: "Flying",
+    selected: False,
+    atk: null,
+    def: 100,
+    sAtk:  120,
+    sDef: 100,
+  }
+  {
+    id: 0359,
+    name: "Absol",
+    type: "Dark",
+    selected: False,
+    atk: null,
+    def: 150,
+    sAtk: 120,
+    sDef: 150,
+  }
+  {
+    id: 0384,
+    name: "Rayquaza",
+    type: "Dragon",
+    selected: False,
+    atk: null,
+    def: 100,
+    sAtk: 250,
+    sDef: 80,
+  }
+  {
+    id: 0399,
+    name: "Bidoof",
+    type: "Normal",
+    selected: False,
+    atk: 1,
+    def: 1,
+    sAtk: null,
+    sDef: 1,
+    // 1% chance if selected to become "The Perfect Bidoof"
+  }
+  /* {
+    id: 0399,
+    name: "The Perfect Bidoof",
+    type: "God",
+    selected: False,
+    atk: 999,
+    def: 999,
+    sAtk: 999,
+    sDef: 999,
+    
+  } */
+  {
+    id: 0493,
+    name: "Arceus",
+    type: "Normal",
+    selected: False,
+    atk: null,
+    def: 300,
+    sAtk: 300,
+    sDef: 300,
+  }
+  {
+    id: 0577,
+    name: "Solosis",
+    type: "Psychic",
+    selected: False,
+    atk: null,
+    def: 50,
+    sAtk: 60,
+    sDef: 100,
+  }
+  {
+    id: 0607,
+    name: "Litwick",
+    type: "Fire",
+    selected: False,
+    atk: null,
+    def: 70,
+    sAtk: 70,
+    sDef: 70,
+  }
+  {
+    id: 0658,
+    name: "Greninja",
+    type: "Water",
+    selected: False,
+    atk: 220,
+    def: 180,
+    sAtk: null,
+    sDef: 100,
+  }
+  {
+    id: 0670,
+    name: "Floette",
+    type: "Fairy",
+    selected: False,
+    atk: null,
+    def: 50,
+    sAtk: 30,
+    sDef: 50,
+  }
+  {
+    id: 0718,
+    name: "Zygarde",
+    type: "Dragon",
+    selected: False,
+    atk: 10,
+    def: 10,
+    sAtk: null,
+    sDef: 10,
+    // gains +10 to all stats for every loss, and +20 for everyone win
+  }
+  {
+    id: 0764,
+    name: "Comfey",
+    type: "Fairy",
+    selected: False,
+    atk: 20,
+    def: 30,
+    sAtk: null,
+    sDef: 30,
+  }
+  {
+    id: 0778,
+    name: "Mimikyu",
+    type: "Ghost",
+    selected: False,
+    atk: null,
+    def: 500,
+    sAtk: 500,
+    sDef: 500,
+    //stats are only 100 if against a ghost type
+  }
+  {
+    id: 0840,
+    name: "Applin",
+    type: "Dragon",
+    selected: False,
+    atk: 10,
+    def: 10,
+    sAtk: null,
+    sDef: 10,
+  }
+  {
+    id: 0869,
+    name: "Alcremie",
+    type: "Fairy",
+    selected: False,
+    atk: null,
+    def: 10,
+    sAtk: 120,
+    sDef: 120,
+  }
+  {
+    id: 0887,
+    name: "Dragapult",
+    type: "Ghost",
+    selected: False,
+    atk: 180,
+    def: 100,
+    sAtk: null,
+    sDef: 100,
+  }
+  {
+    id: 0890,
+    name: "Eternatus",
+    type: "Dragon",
+    selected: False,
+    atk: 250,
+    def: 200,
+    sAtk: null,
+    sDef: 200,
+  }
+  {
+    id: 0908,
+    name: "Meowscarada",
+    type: "Grass",
+    selected: False,
+    atk: 170,
+    def: 100,
+    sAtk: null,
+    sDef: 140,
+  }
+  {
+    id: 0936,
+    name: "Armarouge",
+    type: "Psychic",
+    selected: False,
+    atk: 100,
+    def: 250,
+    sAtk: null,
+    sDef: 150,
+    //if Ceruledge is also in the selectable list, atk becomes 250
+  }
+  {
+    id: 0937,
+    name: "Ceruledge",
+    type: "Ghost",
+    selected: False,
+    atk: 250,
+    def: 100,
+    sAtk: null,
+    sDef: 150,
+    // if Armarouge is also in the selectable list, def becomes 250
+  }
+  {
+    id: 0959,
+    name: "Tinkaton",
+    type: "Fairy",
+    selected: False,
+    atk: 250,
+    def: 50,
+    sAtk: null,
+    sDef: 50,
+  }
+  {
+    id: 0977,
+    name: "Dondozo",
+    type: "Water",
+    selected: False,
+    atk: 150,
+    def: 150,
+    sAtk: null,
+    sDef: 150,
+  }
+  {
+    id: 0978,
+    name: "Tatsugiri",
+    type: "Water",
+    selected: False,
+    atk: 10,
+    def: 10,
+    sAtk: null,
+    sDef: 10
+    //if Dondozo in also in the selectable list, stats all become 400
+  }
+  {
+    id: 0999,
+    name: "Gimighoul",
+    type: "Ghost",
+    selected: False,
+    atk: 25,
+    def: 25,
+    sAtk: null,
+    sDef: 25,
+  }
+  // {
+  //   id: 1000,
+  //   name: "Gholdengo",
+  //   type: "Ghost",
+  //   selected: False,
+  //   atk: 250,
+  //   def: 250,
+  //   sAtk: null,
+  //   sDef: 250,
+  //   //Will only enter list after Gimighoul has 10 wins
+  // }
 ]
